068ee17fe14b564c7182a38a2737741f433239ef
Adam Dybcio <adam.dybcio.kontakt@gmail.com>
2024-06-24 19:33:56 +0200

    Added custom header widget

---
+++ lib/common/widgets/texts/header.dart
@@ -0,0 +1,27 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return FadeInLeft(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: SizedBox(
+          height: size.height * 0.1,
+          width: double.infinity,
+          child: Row(
+            children: [
+              Text(title, style: Theme.of(context).textTheme.labelLarge),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/movies/presentation/pages/home/home_screen.dart
+++ lib/features/movies/presentation/pages/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_bloc_app/common/widgets/movie/custom_movie_backdrop.dart';
 import 'package:movie_bloc_app/features/movies/presentation/widgets/home/custom_movie_carousel.dart';
 
+import '../../../../../common/widgets/texts/header.dart';
 import '../../widgets/home/movie_title.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,18 +10,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Stack(
+          const Stack(
             children: [
               CustomMovieBackdrop(),
               CustomMovieCarousel(),
               MovieTitle(),
             ],
           ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.075),
+          const Header(title: 'Browse Movies'),
         ],
       ),
     );

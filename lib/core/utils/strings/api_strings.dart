42fa75c8e5d8e65ae5b029ad342e68ea809930bc
Adam Dybcio <adam.dybcio.kontakt@gmail.com>
2024-07-09 19:43:28 +0200

    Untracked api strings

--- lib/core/utils/strings/api_strings.dart
+++
@@ -1,6 +0,0 @@
-class ApiStrings {
-  static const String apiKey = 'YOUR_API_KEY';
-  static const String baseUrl = 'https://api.themoviedb.org/3/';
-  static const String imageUrl = 'https://image.tmdb.org/t/p/w500/';
-  static const String youtubeUrl = 'https://www.youtube.com/watch?v=';
-}
